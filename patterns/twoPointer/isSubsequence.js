b6fd748bd287e89e36cd8af830c36b4912ca7af9
andyjzhong <andyjzhong@gmail.com>
2021-10-23 20:32:12 -0400

    practice

--- patterns/twoPointer/isSubsequence.js
+++ patterns/twoPointer/isSubsequence.js
@@ -1,25 +1,19 @@
 function isSubsequence(str1, str2) {
 
-    if (!str1) {
+    let [first, second] = [0, 0]
+
+    for (let i = 0; i < str2.length; i++) {
+        if (str1[first] === str2[second]) {
+            first++
+        }
+        second++
+    }
+
+    if (first === str1.length) {
+        return true
+    } else {
         return false
     }
-
-    let left = 0
-    let right = 0
-
-    while (right < str2.length - 1) {
-        if (str1[left] === str2[right]) {
-            left++
-        }
-
-        if (left === str1.length - 1) {
-            return true
-        }
-
-        right++
-    }
-
-    return false
 }
 
 console.log(isSubsequence("hello", "hello world")) // true
